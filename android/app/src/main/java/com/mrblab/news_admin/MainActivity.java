e02620553be104617e7730c303c1e6ccb1176848
evanabaddon <evanabaddon@gmail.com>
2022-04-12 01:09:06 +0700

    git

---
+++ android/app/src/main/java/com/mrblab/news_admin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mrblab.news_admin;
+
+import io.flutter.embedding.android.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
+                        // Your existing code
+                }
+        );
+    }
+}
---
+++ lib/config/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  final String appName = 'Polres Info'; //app name
+  final String testerPassword =
+      'tester12345'; //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken =
+      'AAAADuCdjuk:APA91bFRoXSe9VtHiUWdftWnv5wwMf0u9NEa92ekEqpos0b9Pprarbx9fF6Splnc3KiCP4aiCk_ppBpaTGV7-kfU_sCWs0QKhML5ZhO9IJ2tWHHGYpExXuN0ADmegRADY-wVhL9iibH2';
+  final String icon = 'assets/images/icon.png'; // app icon
+
+  //don't edit or remove this
+  final List contentTypes = ['image', 'video'];
+}
